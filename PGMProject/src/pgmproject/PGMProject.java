d945bbe8a4ad0f3241265399fd89fd44e1655ed4
AkagamiSama <ben.verdier@hotmail.fr>
2014-12-04 10:46:04 +0100

    Création du GUI.

--- PGMProject/src/pgmproject/PGMProject.java
+++ PGMProject/src/pgmproject/PGMProject.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package pgmproject;
-
+import GUI.*;
 /**
  *
  * @author akagami
@@ -15,13 +15,15 @@ public class PGMProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        PGM a = new PGM();
-        a.lecturePGM("lena.pgm");
-        a.ecriturePGM("lena2");
+        //PGM a = new PGM();
+        //a.lecturePGM("lena.pgm");
+        //a.ecriturePGM("lena2");
         //Histogramme hist = new Histogramme(a);
         //hist.affichage().ecriturePGM("test_hist");
         //Operations.seuillage(a, 127).ecriturePGM("test_seuillage");
-        Operations.difference(a, a).ecriturePGM("test_difference");
+        //Operations.difference(a, a).ecriturePGM("test_difference");
+        GUI window = new GUI();
+        window.setVisible(true);
     }
     
 }
