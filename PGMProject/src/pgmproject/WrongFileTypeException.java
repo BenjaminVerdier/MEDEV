0abeec3d3a2c8da38b8bf2d55c74d56f3252d1c6
FabienFrancois <fabien.francois@eleves.ec-nantes.Fr>
2014-12-04 09:50:44 +0100

    Ajout d'un test pour la méthode EcriturePGM, dans la classe de test PGMTest.

--- PGMProject/src/pgmproject/WrongFileTypeException.java
+++ PGMProject/src/pgmproject/WrongFileTypeException.java
@@ -6,7 +6,8 @@
 package pgmproject;
 
 /**
- *
+ * Exception levée quand on essaye de lire un fichier qui n'est pas au format
+ * PGM avec la méthode lecturePGM.
  * @author Fabien
  */
 public class WrongFileTypeException extends Exception{
